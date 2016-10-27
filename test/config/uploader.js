cc80a7e2d8e8867998e2676a01c62b0dd057258c
calidion <caldiion@gmail.com>
2016-10-28 02:36:06 +0800

    updated

--- test/config/uploader.js
+++ test/config/uploader.js
@@ -2,7 +2,8 @@ var path = require('path');
 module.exports = {
   type: 'disk',
   config: {
-    dir: path.resolve(__dirname, '.hashed/'),                  //文件要保存的地址
+    // 文件要保存的地址
+    dir: path.resolve(__dirname, '.hashed/'),
     base: 'http://localhost'
   }
 };
