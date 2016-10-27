47b29a67baac8ff688eda32f7d3bcb2893a1d53a
calidion <caldiion@gmail.com>
2016-10-28 02:26:04 +0800

    updated

--- test/config/uploader.js
+++ test/config/uploader.js
@@ -1,14 +1,5 @@
 var path = require('path');
 module.exports = {
-  // type: 'oss',
-  // config: {
-  //   accessKeyId: process.env.ALIYUN_OSS_ACCESS_KEY_ID,
-  //   secretAccessKey: process.env.ALIYUN_OSS_ACCESS_KEY_SECRET,
-  //   endpoint: process.env.ALIYUN_OSS_ENDPOINT,
-  //   apiVersion: process.env.ALIYUN_OSS_APP_VERSION,
-  //   Bucket: process.env.ALIYUN_OSS_BUCKET,
-  //   base: process.env.ALIYUN_OSS_BASE
-  // }
   type: 'disk',
   config: {
     dir: path.resolve(__dirname, '.hashed/'),                  //文件要保存的地址
